b524db013bd1270469d01702be6d0af63c065443
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-08-31 23:18:26 -0400

    fixed issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // REQUIRE DEPENDENCIES
-const express = require('express');
-const budgets = require('./models/budgets.js');
-const fruits = require('./models/budgets.js');
+const express = require("express");
+const budgets = require("./models/budgets.js");
+const fruits = require("./models/budgets.js");
 
 // INITIALIZE EXPRESS APP
 const app = express();
@@ -9,36 +9,35 @@ const port = 3000;
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 // DEFINE OUR ROUTES
 
-
 //INDEX
-app.get("/budgets", (rep, res)=>{
+app.get("/budgets", (rep, res) => {
   res.render("index.ejs", {
-        budgets: budgets
-  })
-})
+    budgets: budgets,
+  });
+});
 
 //NEW
-app.get("/budgets/new", (req, res)=>{
-  res.render("new.ejs")
-})
+app.get("/budgets/new", (req, res) => {
+  res.render("new.ejs");
+});
 
 //CREATE
-app.post("/budgets", (req,res)=>{
-  budgets.push(req.body)
-  res.redirect("/budgets")
-})
+app.post("/budgets", (req, res) => {
+  budgets.push(req.body);
+  res.redirect("/budgets");
+});
 
 //SHOW
-app.get("/budgets/:indexOfBudgetsArray", (req, res)=>{
+app.get("/budgets/:indexOfBudgetsArray", (req, res) => {
   res.render("show.ejs", {
-    budgets: budgets[req.params.indexOfBudgetsArray]
-  })
-})
+    budgets: budgets[req.params.indexOfBudgetsArray],
+  });
+});
 
 // TELL OUR APP TO LISTEN ON PORT...
-app.listen(3000, ()=>{
-  console.log(`listening on port `, 3000)
+app.listen(3000, () => {
+  console.log(`listening on port `, 3000);
 });
