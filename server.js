79828c5c4ce7c214eeda89e8cfac46b70d0204d1
Calinpet <92047573+Calinpet@users.noreply.github.com>
2022-08-31 21:43:31 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+// REQUIRE DEPENDENCIES
+const express = require('express');
+const budgets = require('./models/budgets.js');
+const fruits = require('./models/budgets.js');
+
+// INITIALIZE EXPRESS APP
+const app = express();
+const port = 3000;
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+// DEFINE OUR ROUTES
+
+
+//INDEX
+app.get("/budgets", (rep, res)=>{
+  res.render("index.ejs", {
+        budgets: budgets
+  })
+})
+
+//NEW
+app.get("/budgets/new", (req, res)=>{
+  res.render("new.ejs")
+})
+
+//CREATE
+app.post("/budgets", (req,res)=>{
+  budgets.push(req.body)
+  res.redirect("/budgets")
+})
+
+//SHOW
+app.get("/budgets/:indexOfBudgetsArray", (req, res)=>{
+  res.render("show.ejs", {
+    budgets: budgets[req.params.indexOfBudgetsArray]
+  })
+})
+
+// TELL OUR APP TO LISTEN ON PORT...
+app.listen(3000, ()=>{
+  console.log(`listening on port `, 3000)
+});
